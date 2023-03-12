b16e5396b579e9e0b84c3d2da5c8e65f35d2a4b3
Chris Jones <redefinescience@hotmail.com>
2023-03-12 18:25:51 -0400

    oops, going to try again

--- ksoc-client-lib/src/commonMain/kotlin/com/kotlineering/ksoc/client/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlineering.ksoc.client
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- ksoc-client-lib/src/commonMain/kotlin/com/kotlineering/ksoc/client/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.kotlineering.ksoc.client
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
